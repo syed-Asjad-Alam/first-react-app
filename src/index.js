7056fb81c4799a20f48c9560ca2c2a4489aac2fc
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-09-13 21:37:54 +0500

    Did more tasks
    Completed Room task, ON OFF task and temperature task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,12 +6,64 @@ import MediaCard from './mediaCard';
 import Gate from './gate';
 import reportWebVitals from './reportWebVitals';
 
+function Room() {
+  const [isLit, setLit] = useState(true);
+  const lightedness = isLit ? "lit" : "dark";
+
+
+  return (
+    <div className={`room ${lightedness}`}>The Room is {lightedness}
+    <br/>
+    <button onClick={() => setLit(!isLit)}>flip</button>
+    <br/>
+    </div>
+  )
+}
+function ONOFF() {
+  const [isGreen, setGreen] = useState();
+  return(
+    <div className={`ONOFF ${isGreen ? 'Green': 'notGreen'}`}>
+      <button onClick={() => setGreen(true)}>ON</button>
+      <button onClick={() => setGreen(false)}>OFF</button>
+    </div>
+  )
+}
+
+function Temprature() {
+  const [temp , settemp] = useState(22);
+  function inctemp() {
+    settemp(prevtemp => prevtemp + 1)
+  }
+
+  function dectemp() {
+    settemp(prevtemp => prevtemp - 1)
+  }
+
+  return(
+    <div>
+      <button onClick={dectemp}>-</button>
+      &nbsp;&nbsp;&nbsp;
+      <span>{temp}</span>
+      &nbsp;&nbsp;&nbsp;
+      <button onClick={inctemp}>+</button>   
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <div>
-  <App name = "Asjad Alam" age = {21}/>,
-  <MediaCard title="Mediacard Task" body="This is the Body of mediacard task" imageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pakistan_Monument_Ibd.jpg" />,
-  <Gate isOpen={true} />,
-  <Gate isOpen={false} />,
+  <div style={{margin:"auto", width:"50%"}}>
+  <App name = "Asjad Alam" age = {21}/>
+  <MediaCard title="Mediacard Task" body="This is the Body of mediacard task" imageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pakistan_Monument_Ibd.jpg" />
+  <Gate isOpen={true} />
+  <Gate isOpen={false} />
+  <br/>
+  <br/>
+  <Room/>
+  <br/>
+  <ONOFF/>
+  <br/>
+  <br/>
+  <Temprature/>
   </div>,
   document.getElementById('root')
 )
